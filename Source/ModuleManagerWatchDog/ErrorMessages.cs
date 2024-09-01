9c69c0f0d2a806e69c2d4a7a282d2af76effca72
Lisias T <me@lisias.net>
2024-08-31 22:52:58 -0300

    Some repharsing on the Message Boxes, in a (futile, probably) attempt to prevent this [kind of crap](https://www.reddit.com/r/KerbalAcademy/comments/1ejaf9b/houstonerror_contradiction/)

--- Source/ModuleManagerWatchDog/ErrorMessages.cs
+++ Source/ModuleManagerWatchDog/ErrorMessages.cs
@@ -20,12 +20,12 @@ namespace WatchDog.ModuleManager
 	{
 		public static readonly string ERR_MMWD_DUPLICATED = "There're more than one MM Watch Dog on this KSP installment! Please delete all but the one you intend to use!";
 		public static readonly string ERR_MMWD_WRONGPLACE = "666_ModuleManagerWatchDog.dll <b>must be</b> directly on GameData and not inside any subfolder (i.e., it must be in the same place ModuleManager.dll is). Please move 666_ModuleManagerWatchDog.dll directly into GameData.";
-		public static readonly string ERR_MM_ABSENT = "There's no Module Manager on this KSP installment! You need to install Module Manager!";
+		public static readonly string ERR_MM_ABSENT = "Module Manager couldn't be found on this KSP installment! It may be due a faulty installation, but also due the (in)famous <i>Assembly Loader/Resolver</i> bug!";
 		public static readonly string ERR_MM_WRONGPLACE = "ModuleManager.dll <b>must be</b> directly on GameData and not inside any subfolder. Please move ModuleManager.dll directly into GameData.";
 		public static readonly string ERR_MM_DOPPELGANGER = "There're more than one Module Manager on this KSP installment! Please delete all but the one you intend to use!";
 		public static readonly string ERR_MM_FORUMDELETED = "You had selected MM/L as preferred Module Manager, but somehow other forks were installed. They were automatically removed.";
 		public static readonly string ERR_MM_MYFORKDELETED = "You had selected Forum's as preferred Module Manager, but somehow other forks were installed. They were automatically removed.";
-		public static readonly string ERR_MM_MISSING_DEPENDENCIES = "You have MM installed, but not its dependencies. If using MM/L, you need to install KSPe too!"; 
+		public static readonly string ERR_MM_MISSING_DEPENDENCIES = "You have MM installed, but its dependencies weren't found. It may be due a faulty installation, but also due the (in)famous <i>Assembly Loader/Resolver</i> bug!"; 
 
 		internal static class Conflict
 		{
